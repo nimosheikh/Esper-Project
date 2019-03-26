8e2cbd26e6ed0fe59449c7fcdcdd1ba4a2b9d7a8
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 21:54:53 +1100

    Create industry-Data.php

---
+++ industry-Data.php
@@ -0,0 +1,10 @@
+
+
+•	In Canvas, you will find a link to some industry data supplied by Burning Glass. You should use this (as well as any other data you may be able to find) to answer the following questions.
+•	What are the Job Titles for your group's ideal jobs? How do each of these rank in terms of demand from employers?
+•	From your group's ideal jobs, you can identify a set of skills required for these jobs (we will refer to this as your group's required skill set). These can be divided into general skills (communication, problem solving, writing etc) and IT-specific skills (Javascript, SQL, etc).
+•	How do the IT-specific skills in your required skill set rank in terms of demand from employers?
+•	How do the general skills in your required skill set rank in terms of demand from employers?
+•	What are the three highest ranked IT-specific skills which are not in your required skill set?
+•	What are the three highest ranked general skills which are not in your required skill set?
+•	Having looked at the Burning Glass data, has your opinion of your ideal job changed? Why or why not?
