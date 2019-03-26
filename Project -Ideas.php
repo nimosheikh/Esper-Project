6a6fe7fb80f18e0f5b13aae2fd9e5ec292e6f1b0
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 22:07:30 +1100

    Create Project -Ideas.php

---
+++ Project -Ideas.php
@@ -0,0 +1,15 @@
+
+
+You are to come up with a project idea for the group as a whole, which could be an individual’s idea, but is more likely to be
+a combination of each of the ideas you had individually, together with comments and feedback from your Assignment 1 marker, 
+instructor, and anyone else who you would like to consult.
+•	You may also wish to consider what problems need to be solved, for which there are many sources of information,
+such as the ones below. There are many more websites like these, of course.
+•	https://www.weforum.org/agenda/2016/01/what-are-the-10-biggest-global-challenges/ 
+
+•	https://www.cheatsheet.com/money-career/want-to-be-a-billionaire-solve-one-of-these-5-problems.html/?a=viewall 
+•	https://www.quora.com/What-are-common-problems-that-need-solving
+•	There is no set length for this section, but it is difficult to see how a description of less than 500 words would be adequate.
+Do not limit yourself to this if you have more than this to write, though - but it should be in a well written and concise format. 
+If you are unsure of how much to write, ask your instructor for advice.
+•	Feedback
