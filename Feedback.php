fe906dd1ae5a33e7a71aca8f1759f6f4a0cd6029
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 22:10:10 +1100

    Create Feedback.php

---
+++ Feedback.php
@@ -0,0 +1,11 @@
+•	A key feature of industry teams is the ability to give and receive feedback
+•	 particular to members of a team in which you are working. In order to give you some experience at this, your group
+should login to the SparkPLUS tool (instructions on how to do this 
+•	will be given on Canvas). Each person should provide an assessment of each member of the group, including themselves. 
+This will then provide feedback to each of you, and in particular, on how the rest of the team view’s your performance.
+•	You do not need to include any information about the feedback you receive, as this is intended to give you experience
+with use of tools such as these, and how the information is gathered and processed in such tools. The markers will, however, 
+check that each person in the group has contributed via SparkPLUS, and the marks for this section will only be awarded if
+all group members have contributed in an appropriate manner by the assignment deadline.
+•	If you have any concerns with this process, please raise these with your tutor or lecturer. In particular, if you wish
+to have a group discussion about this feedback, please contact the lecturer who will facilitate a group discussion on this topic.
