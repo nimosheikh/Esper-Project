bf10b4c3441fc3b51d3fa8aca3d5a5600dbaac0b
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 21:47:32 +1100

    Create Overview.php

---
+++ Overview.php
@@ -0,0 +1,13 @@
+
+Introduction to Information Technology
+1.	Assessment Overview
+•	You have clearly described what worked well in your group.
+•	You have discussed what could be improved. 
+•	You have included at least one surprising thing. 
+•	You have appropriately discussed one thing you have learned about groups.
+•	Your report looks clean and professional
+•	and uses appropriate font
+•	colours and backgrounds.
+•	You have included an appropriate number of images.
+•	produce a report on various aspects of the IT industry 
+•	plan for an IT project.
