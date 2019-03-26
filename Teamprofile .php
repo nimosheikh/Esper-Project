2077c76a6f610ae7be3370ee514e386637634850
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 21:49:22 +1100

    Create Teamprofile .php

---
+++ Teamprofile .php
@@ -0,0 +1,9 @@
+
+•	Purpose  as a team to produce a report on various aspects of the IT industry.
+•	You’ll demonstrate your ability to work in a team environment
+•	complete research tasks
+•	and to present information in a professional report format.
+•	You’ll also explore your creative side by putting forward a group project idea expanding or combining project ideas
+•	Writing up a document covering all of the different areas listed above (and detailed below)
+•	formatted and presented in a PDF Report. Present a group website hosted on GitHub Pages.
+
