c0a01de2668e35f4d63ecb1b4408c3fa17004bab
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 22:14:13 +1100

    Create Referencing.php

---
+++ Referencing.php
@@ -0,0 +1,9 @@
+
+•	Use harvard  for this assessment if appropriate.
+•	You must acknowledge all the courses of information you have used in your assessments. 
+•	Refer to the RMIT Easy Cite referencing tool to see examples and tips on how to reference in the appropriated style. 
+You can also refer to the library referencing page for more tools such as EndNote, referencing tutorials and referencing guides
+for printing. 
+•	Please note: applications must be submitted at least one business day prior to the due date. (e.g. If an assessment is due
+on Sunday, you will need to submit a form by Friday).
+
