24a9d40bd0b53c71f116d6f4ebe066f53be6b5c9
nimosheikh <48153205+nimosheikh@users.noreply.github.com>
2019-03-26 21:52:18 +1100

    Create  Team-specifications.php

---
+++ Team-specifications.php
@@ -0,0 +1,23 @@
+
+
+•	Team Name
+•	Personal information
+•	One paragraph per person, including name, student number, background, hobbies, IT interest and IT experience. 
+•	include your team’s chosen name.
+•	The test outcomes for each person in the team (all 3 tests), and your understanding of how this information may be helpful to the group. 
+•	Compare and contrast the ideal jobs for each person in the group.
+•	What common elements are there, if any? What differentiates each position from the others, if anything? 
+•	How similar or different are your career plans across the group?
+•	include links to each individual website on the group website.
+•	You also need to set up a GitHub or similar repository for your group on which you can use Git.
+•	This is to facilitate the production and maintenance of group artefacts, which will include the assignment submission
+•	reports or information
+•	Code
+•	Examples
+•	formatted data, images and videos, links, and similar information.
+•	You will also need to set up your group on Canvas.
+•	In your report you should include a brief description of what you have done, and include the following:
+•	The link to your group’s website
+•	The link to your group’s Git repository (GitHub, BitBucket, etc)
+•	Your comments on how well the audit trail on the Git repository reflects your group’s work. You will presumably only be able to do this close to the time of submission.
+
